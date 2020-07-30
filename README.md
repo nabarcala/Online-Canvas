76d069cab42704508d2cbfe13d904caccee4cbf4
Natacha <nabarcala@gmail.com>
2020-07-29 20:57:11 -0400

    Changed color picker and fixed toolbar to look better

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Online-Canvas
+
+A canvas accessible from the web. Draw anything you want!
+
+### Features:
+* Change brush color
+* Change brush size
+
+# Acknowledgement
+* [Simonwep's pickr](https://github.com/Simonwep/pickr#getting-started) - Flat, Simple, Hackable Color-Picker.
--- js/canvas.js
+++ js/canvas.js
@@ -17,6 +17,51 @@ var currentColor = document.querySelector('#color-picker');
 var BrushSize = document.querySelector('#brush-size');
 var BrushSizeLabel = document.querySelector('#current-brush-size');
 
+// Simple example, see optional options for more configuration.
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+    default: 'black',
+    closeWithKey: 'Enter',
+    padding: 21,
+    swatches: [
+        '#000000',
+        '#fff',
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: false
+        }
+    }
+});
+
 window.addEventListener('load', () => {
 
     // Set length and width of canvas
@@ -50,8 +95,15 @@ window.addEventListener('load', () => {
 });
 
 // Event Listeners
-currentColor.addEventListener('input', ChangeColor);
+// currentColor.addEventListener('input', ChangeColor);
 BrushSize.addEventListener('input', ChangeBrushSize);
+
+// Change color of the brush
+pickr.on('change', (color, instance) => {
+    let cc = color.toRGBA().toString();
+    brush.color = cc;
+    pickr.setColor(cc);
+});
 
 // Change color of the brush
 function ChangeColor() {
@@ -62,3 +114,7 @@ function ChangeBrushSize() {
     brush.size = BrushSize.value;
     BrushSizeLabel.innerHTML = brush.size;
 }
+
+
+
+
